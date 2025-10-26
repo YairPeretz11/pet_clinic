511349d64dd525b5aaf51a34b96d3f82c11228b2
Yair Peretz <yairperetz11@gmail.com>
2025-10-26 18:01:07 +0200

    .

--- lib/interfaces.ts
+++ lib/interfaces.ts
@@ -3,8 +3,7 @@ export interface IPatient {
   phone: string;
   petName: string;
   petType: string;
-  // Source of truth saved in DB
   birthDate: string | Date;
-  // Derived on the server/client for display/filtering
+  // Derived field
   petAge?: number;
 }
